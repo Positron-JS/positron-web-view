bf0f0660ddfdb6245b98f99cf302e81f0fba82f9
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-08-23 21:08:00 +0530

    wip

---
+++ Positron/Resources/Scripts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuroSpeech.Positron.Resources
+{
+    internal class Scripts
+    {
+
+        public static string Read(string name)
+        {
+            using var s = typeof(Scripts).Assembly.GetManifestResourceStream("NeuroSpeech.Positron.Resources." + name);
+            var reader = new StreamReader(s!);
+            return reader.ReadToEnd();
+        }
+
+        public static string TSLib => Read("TSLib.js");
+
+        public static string Positron => Read("Positron.js");
+
+    }
+}
